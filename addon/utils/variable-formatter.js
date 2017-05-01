2b669951bebbc8926532ff6a28540edfd655eb6b
David Billing <david.j.billing@gmail.com>
2017-05-01 16:54:36 +0200

    Added more transormations

--- addon/utils/variable-formatter.js
+++ addon/utils/variable-formatter.js
@@ -29,6 +29,42 @@ export default function variableFormatter(options) {
   if(options.default_image){
     variables.push('d_' + options.default_image);
   }
+  if(options.aspect_ratio){
+    variables.push('ar_' + options.aspect_ratio);
+  }
+  if(options.gravity){
+    variables.push('g_' + options.gravity);
+  }
+  if(options.zoom){
+    variables.push('z_' + options.zoom);
+  }
+  if(options.x){
+    variables.push('x_' + options.x);
+  }
+  if(options.y){
+    variables.push('y_' + options.y);
+  }    
+  if(options.angle){
+    variables.push('a_' + options.angle);
+  } 
+  if(options.effect){
+    variables.push('e_' + options.effect);
+  }
+  if(options.opacity){
+    variables.push('o_' + options.opacity);
+  }
+  if(options.border){
+    variables.push('bo_' + options.border);
+  }
+  if(options.background){
+    variables.push('b_' + options.background);
+  }
+  if(options.overlay){
+    variables.push('l_' + options.overlay);
+  }
+  if(options.underlay){
+    variables.push('u_' + options.underlay);
+  }
 
   return '/' + variables.join(",");
 }
