4d13a672c974224ec0b12b7662afba7a9719df8d
David Billing <david.j.billing@gmail.com>
2018-10-16 18:35:41 +0200

    Update ember-cli 3.5

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,3 +1,5 @@
+/*eslint no-undef: "error"*/
+/*eslint-env node*/
 module.exports = {
   root: true,
   parserOptions: {
--- tests/.eslintrc.js
+++ tests/.eslintrc.js
@@ -1,3 +1,5 @@
+/*eslint no-undef: "error"*/
+/*eslint-env node*/
 module.exports = {
   env: {
     embertest: true
