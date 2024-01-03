a5379e5a43660acbefb04537bf8e6de94521d0d6
David Billing <david.j.billing@gmail.com>
2024-01-03 11:23:19 +0100

    fix embroider build

--- tests/dummy/config/targets.js
+++ tests/dummy/config/targets.js
@@ -4,7 +4,6 @@ const browsers = [
   'last 1 Chrome versions',
   'last 1 Firefox versions',
   'last 1 Safari versions',
-  'last 1 Edge versions',
 ];
 
 module.exports = {
--- tests/helpers/index.js
+++ tests/helpers/index.js
@@ -1,42 +1,42 @@
 import {
-  setupApplicationTest,
-  setupRenderingTest,
-  setupTest,
+  setupApplicationTest as upstreamSetupApplicationTest,
+  setupRenderingTest as upstreamSetupRenderingTest,
+  setupTest as upstreamSetupTest,
 } from 'ember-qunit';
 
 // This file exists to provide wrappers around ember-qunit's
 // test setup functions. This way, you can easily extend the setup that is
 // needed per test type.
 
-// function mySetupApplicationTest(hooks, options) {
-//   setupApplicationTest(hooks, options);
+function setupApplicationTest(hooks, options) {
+  upstreamSetupApplicationTest(hooks, options);
 
-//   // Additional setup for application tests can be done here.
-//   //
-//   // For example, if you need an authenticated session for each
-//   // application test, you could do:
-//   //
-//   // hooks.beforeEach(async function () {
-//   //   await authenticateSession(); // ember-simple-auth
-//   // });
-//   //
-//   // This is also a good place to call test setup functions coming
-//   // from other addons:
-//   //
-//   // setupIntl(hooks); // ember-intl
-//   // setupMirage(hooks); // ember-cli-mirage
-// }
+  // Additional setup for application tests can be done here.
+  //
+  // For example, if you need an authenticated session for each
+  // application test, you could do:
+  //
+  // hooks.beforeEach(async function () {
+  //   await authenticateSession(); // ember-simple-auth
+  // });
+  //
+  // This is also a good place to call test setup functions coming
+  // from other addons:
+  //
+  // setupIntl(hooks); // ember-intl
+  // setupMirage(hooks); // ember-cli-mirage
+}
 
-// function mySetupRenderingTest(hooks, options) {
-//   setupRenderingTest(hooks, options);
+function setupRenderingTest(hooks, options) {
+  upstreamSetupRenderingTest(hooks, options);
 
-//   // Additional setup for rendering tests can be done here.
-// }
+  // Additional setup for rendering tests can be done here.
+}
 
-// function mySetupTest(hooks, options) {
-//   setupTest(hooks, options);
+function setupTest(hooks, options) {
+  upstreamSetupTest(hooks, options);
 
-//   // Additional setup for unit tests can be done here.
-// }
+  // Additional setup for unit tests can be done here.
+}
 
 export { setupApplicationTest, setupRenderingTest, setupTest };
